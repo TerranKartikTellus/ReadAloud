5327bb7a6f1c5d5fc27650bfb3dfc6b7d5fa18b8
Terran Kartik Tellus <91951646+TerranKartikTellus@users.noreply.github.com>
2023-01-14 10:40:53 +0530

    first commit

---
+++ main1.py
@@ -0,0 +1,19 @@
+import win32com.client as wincl
+import os
+
+
+def text_to_speech(text_file):
+    with open(text_file, 'r') as f:
+        text = f.read()
+
+    # Initialize the speech synthesizer
+    speak = wincl.Dispatch("SAPI.SpVoice")
+    speak.Rate = 4
+    voices = speak.GetVoices()
+    for voice in voices:
+        print(voice.GetDescription())
+    speak.Voice = voices.Item(1)
+    speak.Speak(text)
+
+
+text_to_speech("text.txt")
